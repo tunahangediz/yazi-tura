ba7955d6295191c64c296293aea3026e519a4f8d
Tunahan Gediz <tunahangediz@Tunahan-MacBook-Pro.local>
2021-04-03 16:50:47 +0300

    new commit

--- src/components/CoinFlipper/CoinFlipper.js
+++ src/components/CoinFlipper/CoinFlipper.js
@@ -1,41 +1,45 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import Coin from "../Coin/Coin";
 import "./CoinFlipper.css";
-
-class CoinFlipper extends Component {
-  constructor(props) {
-    super(props);
-    // State üzerinde paranın başlangıçtaki durumunu veriyoruz, başlangıçta "tura" olsun.
-    // Daha sonra şu anda paranın dönüp dönmeme durumunu da veriyoruz, başlangıçta para atılmamış olduğundan "false" olarak verdik.
-    this.state = {
-      side: "tura",
-      flipping: false,
+const CoinFlipper = () => {
+    const [side, setSide] = useState("tura");
+    const [flipping, setFlipping] = useState(false);
+    const [count, setCounter] = useState(0);
+    const [sonuclar, setSonuc] = useState([]);
+    const handleClick = () => {
+        // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "flipping" durumunu "true" yapıyoruz.
+        setFlipping(true);
+        let yuz = yaziTura();
+        setCounter(count + 1);
+        setSide(yuz);
+        setSonuc([...sonuclar, yuz]);
+        // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "flipping" durmunu tekrar "false" yapıyoruz.
+        setTimeout(() => {
+            setFlipping(false);
+        }, 1000);
     };
-  }
-  handleClick = () => {
-    // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "flipping" durumunu "true" yapıyoruz.
-    this.setState({ flipping: true });
-    // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "flipping" durmunu tekrar "false" yapıyoruz.
-    setTimeout(() => this.setState({ flipping: false }), 1000);
-  };
-
-  render() {
+    function toplamTura() {
+        const arr = sonuclar.filter((sonuc) => sonuc == "tura");
+        return arr.length;
+    }
+    function yaziTura() {
+        const sayi = Math.round(Math.random() * 1);
+        return sayi == 0 ? "yazi" : "tura";
+    }
     return (
-      <div className="CoinFlipper">
-        <h1>Yazı mı Tura mı?</h1>
-        <Coin side={this.state.side} flipping={this.state.flipping} />
-        <button onClick={this.handleClick}>At!</button>
-        <p>
-          Toplam
-          <strong> 5 </strong>
-          atıştan
-          <strong> 3 </strong>ü tura
-          <strong> 2 </strong>
-          si yazı geldi.
-        </p>
-      </div>
+        <div className="CoinFlipper">
+            <h1>Yazı mı Tura mı?</h1>
+            <Coin side={side} flipping={flipping} />
+            <button onClick={handleClick}>At!</button>
+            <p>
+                Toplam
+                <strong> {count} </strong>
+                atıştan
+                <strong> {toplamTura()}</strong>ü tura
+                <strong> {count - toplamTura()} </strong>
+                si yazı geldi.
+            </p>
+        </div>
     );
-  }
-}
-
+};
 export default CoinFlipper;
